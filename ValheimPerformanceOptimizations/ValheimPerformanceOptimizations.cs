9e21027e6ef0b34954f5961d6b0e1be0c4f4799c
ontrigger <firewarpcd@gmail.com>
2021-06-11 02:24:10 +0300

    initial

---
+++ ValheimPerformanceOptimizations/ValheimPerformanceOptimizations.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using BepInEx;
+using HarmonyLib;
+
+namespace ValheimPerformanceOptimizations
+{
+    [BepInPlugin(PluginId, "Valheim Performance Optimizations", "0.0.1")]
+    [BepInDependency("moreslots", BepInDependency.DependencyFlags.SoftDependency)]
+    public class ValheimPerformanceOptimizations : BaseUnityPlugin
+    {
+        public const string PluginId = "dev.ontrigger.vpo";
+
+        private static ValheimPerformanceOptimizations _instance;
+        private Harmony _harmony;
+
+        private void Awake()
+        {
+            _instance = this;
+
+            _harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), PluginId);
+        }
+
+        private void OnDestroy()
+        {
+            _harmony?.UnpatchAll(PluginId);
+        }
+    }
+}
