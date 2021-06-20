32ea2f1cdda350b838319b72e66a20ad5dd2597d
ontrigger <firewarpcd@gmail.com>
2021-06-20 19:50:29 +0300

    push grass rendering to a later step to reduce pixel overdraw

---
+++ ValheimPerformanceOptimizations/ClutterSystemPatches.cs
@@ -0,0 +1,21 @@
+using HarmonyLib;
+using UnityEngine.Rendering;
+
+namespace ValheimPerformanceOptimizations
+{
+    [HarmonyPatch]
+    public class ClutterSystemPatches
+    {
+        [HarmonyPatch(typeof(ClutterSystem), "Awake")]
+        private static void Postfix(ClutterSystem __instance)
+        {
+            foreach (var clutter in __instance.m_clutter)
+            {
+                var renderer = clutter.m_prefab.GetComponent<InstanceRenderer>();
+                if (!renderer) continue;
+
+                renderer.m_material.renderQueue = (int) RenderQueue.AlphaTest;
+            }
+        }
+    }
+}
