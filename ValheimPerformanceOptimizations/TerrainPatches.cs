ec2a8178d57906aaff7894c92d2744c389a3b329
ontrigger <firewarpcd@gmail.com>
2021-06-22 18:24:33 +0300

    GetWaterLevel perf improvements move stuff around

--- ValheimPerformanceOptimizations/TerrainPatches.cs
+++ ValheimPerformanceOptimizations/TerrainPatches.cs
@@ -46,40 +46,4 @@ namespace ValheimPerformanceOptimizations
             ___m_light.cullingMask &= ~(1 << TerrainLodLayer);
         }
     }
-
-    /// <summary>
-    ///     The water planes are being rendered even when they are fully below the terrain
-    ///     This patch makes water render only if the lowest point of its terrain intersects with the water level
-    /// </summary>
-    [HarmonyPatch]
-    public static class WaterVolumeVisibilityPatch
-    {
-        private static readonly ConditionalWeakTable<Heightmap, Action<Bounds>> HeightmapChangedCallbacks =
-            new ConditionalWeakTable<Heightmap, Action<Bounds>>();
-
-        [HarmonyPatch(typeof(Heightmap), "Regenerate")]
-        private static void Postfix(Heightmap __instance, Bounds ___m_bounds)
-        {
-            Action<Bounds> action;
-            if (HeightmapChangedCallbacks.TryGetValue(__instance, out action))
-            {
-                action.Invoke(___m_bounds);
-            }
-        }
-
-        [HarmonyPatch(typeof(WaterVolume), "Awake")]
-        private static void Postfix(WaterVolume __instance)
-        {
-            if (!__instance.m_heightmap || !__instance.m_collider)
-            {
-                return;
-            }
-
-            HeightmapChangedCallbacks.Add(__instance.m_heightmap, heightmapBounds =>
-            {
-                var waterLevel = ZoneSystem.m_instance.m_waterLevel + 1f;
-                __instance.m_waterSurface.enabled = waterLevel >= heightmapBounds.min.y;
-            });
-        }
-    }
 }
