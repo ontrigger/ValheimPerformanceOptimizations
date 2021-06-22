f073862bbe7632727cab939b6409646b2105a0e2
ontrigger <firewarpcd@gmail.com>
2021-06-22 18:37:53 +0300

    bump to 0.2.0

--- ValheimPerformanceOptimizations/ValheimPerformanceOptimizations.cs
+++ ValheimPerformanceOptimizations/ValheimPerformanceOptimizations.cs
@@ -5,7 +5,7 @@ using HarmonyLib;
 
 namespace ValheimPerformanceOptimizations
 {
-    [BepInPlugin(PluginId, "Valheim Performance Optimizations", "0.1.0")]
+    [BepInPlugin(PluginId, "Valheim Performance Optimizations", "0.2.0")]
     public class ValheimPerformanceOptimizations : BaseUnityPlugin
     {
         public const string PluginId = "dev.ontrigger.vpo";
