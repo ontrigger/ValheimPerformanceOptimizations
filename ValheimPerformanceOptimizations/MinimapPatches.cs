5258233f596f8998d08043d755a33ec6b6e7a6f9
Maximilian Schmöcker <m.schmoecker@brickfire.eu>
2021-06-23 19:18:24 +0200

    pack all minimap textures in one file and compress it

--- ValheimPerformanceOptimizations/MinimapPatches.cs
+++ ValheimPerformanceOptimizations/MinimapPatches.cs
@@ -1,4 +1,5 @@
 ﻿using System.IO;
+using System.IO.Compression;
 using HarmonyLib;
 using UnityEngine;
 
@@ -7,8 +8,7 @@ namespace ValheimPerformanceOptimizations
     /// <summary>
     /// Generating the minimap takes a lot of time at startup and is generated from WorldGenerator. Therefore the
     /// minimap never changes in a given world.
-    /// Now it is being saved inside the world folder as PNGs (there are multiple for different masks). The file names
-    /// are worldName_worldSeed_gameVersion_textureType.png
+    /// Now it is being saved inside the world folder as a zipped file with the name worldName_worldSeed_gameVersion.map
     /// </summary>
     [HarmonyPatch(typeof(Minimap), "GenerateWorldMap")]
     public static class MinimapPatches
@@ -16,11 +16,9 @@ namespace ValheimPerformanceOptimizations
         private static bool Prefix(Minimap __instance)
         {
             // try to load existing textures
-            if (ImageFilesExists())
+            if (File.Exists(FilePath()))
             {
-                __instance.m_forestMaskTexture.LoadImage(File.ReadAllBytes(ForestMaskTexturePath()));
-                __instance.m_mapTexture.LoadImage(File.ReadAllBytes(MapTexturePath()));
-                __instance.m_heightTexture.LoadRawTextureData(File.ReadAllBytes(HeightTexturePath()));
+                LoadFromFile(__instance);
                 return false;
             }
 
@@ -30,46 +28,56 @@ namespace ValheimPerformanceOptimizations
 
         private static void Postfix(Minimap __instance)
         {
-            if (!ImageFilesExists())
+            if (!File.Exists(FilePath()))
             {
-                // write computed files to file
-                Directory.CreateDirectory(GetMinimapSavePath());
-                File.WriteAllBytes(ForestMaskTexturePath(), __instance.m_forestMaskTexture.EncodeToPNG());
-                File.WriteAllBytes(MapTexturePath(), __instance.m_mapTexture.EncodeToPNG());
-                File.WriteAllBytes(HeightTexturePath(), __instance.m_heightTexture.GetRawTextureData());
+                // write computed textures to file
+                Directory.CreateDirectory(MinimapSaveFolderPath());
+                SaveToFile(__instance);
             }
         }
 
-        public static bool ImageFilesExists()
+        private static void SaveToFile(Minimap minimap)
         {
-            return File.Exists(ForestMaskTexturePath()) &&
-                   File.Exists(MapTexturePath()) &&
-                   File.Exists(HeightTexturePath());
+            using (FileStream fileStream = File.Create(FilePath()))
+            using (GZipStream compressionStream = new GZipStream(fileStream, CompressionMode.Compress))
+            {
+                ZPackage package = new ZPackage();
+                package.Write(minimap.m_forestMaskTexture.GetRawTextureData());
+                package.Write(minimap.m_mapTexture.GetRawTextureData());
+                package.Write(minimap.m_heightTexture.GetRawTextureData());
+
+                byte[] data = package.GetArray();
+                compressionStream.Write(data, 0, data.Length);
+            }
         }
 
-        public static string GetMinimapSavePath()
+        private static void LoadFromFile(Minimap minimap)
+        {
+            using (FileStream fileStream = File.OpenRead(FilePath()))
+            using (GZipStream decompressionStream = new GZipStream(fileStream, CompressionMode.Decompress))
+            using (MemoryStream resultStream = new MemoryStream())
+            {
+                decompressionStream.CopyTo(resultStream);
+                ZPackage package = new ZPackage(resultStream.ToArray());
+
+                minimap.m_forestMaskTexture.LoadRawTextureData(package.ReadByteArray());
+                minimap.m_forestMaskTexture.Apply();
+                minimap.m_mapTexture.LoadRawTextureData(package.ReadByteArray());
+                minimap.m_mapTexture.Apply();
+                minimap.m_heightTexture.LoadRawTextureData(package.ReadByteArray());
+                minimap.m_heightTexture.Apply();
+            }
+        }
+
+        public static string FilePath()
+        {
+            string file = ZNet.m_world.m_name + "_" + ZNet.m_world.m_seed + "_" + Version.GetVersionString() + ".map";
+            return MinimapSaveFolderPath() + "/" + file;
+        }
+
+        public static string MinimapSaveFolderPath()
         {
             return World.GetWorldSavePath() + "/minimap";
-        }
-
-        public static string GetBaseFileName()
-        {
-            return ZNet.m_world.m_name + "_" + ZNet.m_world.m_seed + "_" + Version.GetVersionString();
-        }
-
-        public static string ForestMaskTexturePath()
-        {
-            return GetMinimapSavePath() + "/" + GetBaseFileName() + "_forestMaskTexture.png";
-        }
-
-        public static string MapTexturePath()
-        {
-            return GetMinimapSavePath() + "/" + GetBaseFileName() + "_mapTexture.png";
-        }
-
-        public static string HeightTexturePath()
-        {
-            return GetMinimapSavePath() + "/" + GetBaseFileName() + "_heightTexture.raw";
         }
     }
 }
