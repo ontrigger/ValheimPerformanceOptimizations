9dce6c7353da2b0084d096ca9c1f2927b8611770
ontrigger <firewarpcd@gmail.com>
2021-07-05 18:24:19 +0300

    whoops lol

--- ValheimPerformanceOptimizations/ValheimPerformanceOptimizations.cs
+++ ValheimPerformanceOptimizations/ValheimPerformanceOptimizations.cs
@@ -6,7 +6,7 @@ using ValheimPerformanceOptimizations.Patches;
 
 namespace ValheimPerformanceOptimizations
 {
-    [BepInPlugin(PluginId, "Valheim Performance Optimizations PREVIEW", "0.5.0")]
+    [BepInPlugin(PluginId, "Valheim Performance Optimizations", "0.5.0")]
     public class ValheimPerformanceOptimizations : BaseUnityPlugin
     {
         public const string PluginId = "dev.ontrigger.vpo";
