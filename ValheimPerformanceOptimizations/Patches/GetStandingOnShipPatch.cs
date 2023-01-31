eae80271c0e8922209cc7fede778c05ab6d34d4b
ontrigger <firewarpcd@gmail.com>
2023-01-31 03:09:54 +0300

    simplify standingonship patch, idk why i did it that way

--- ValheimPerformanceOptimizations/Patches/GetStandingOnShipPatch.cs
+++ ValheimPerformanceOptimizations/Patches/GetStandingOnShipPatch.cs
@@ -1,4 +1,4 @@
-using System.Runtime.CompilerServices;
+using System.Collections.Generic;
 using BepInEx.Configuration;
 using HarmonyLib;
 using UnityEngine;
@@ -8,8 +8,7 @@ namespace ValheimPerformanceOptimizations.Patches
 {
     public static class GetStandingOnShipPatch
     {
-        private static readonly ConditionalWeakTable<Character, CachedShipData> Data =
-            new ConditionalWeakTable<Character, CachedShipData>();
+        private static readonly Dictionary<Character, Ship> LastShipByCharacter = new();
 
         static GetStandingOnShipPatch()
         {
@@ -24,51 +23,57 @@ namespace ValheimPerformanceOptimizations.Patches
             }
         }
 
-        private static CachedShipData GetCachedShipData(this Character character)
+        [HarmonyPatch(typeof(Character), nameof(Character.OnDestroy)), HarmonyPostfix]
+        private static void OnDestroy_Postfix(Character __instance)
         {
-            return Data.GetOrCreateValue(character);
+	        LastShipByCharacter.Remove(__instance);
         }
 
-        [HarmonyPatch(typeof(Character), nameof(Character.GetStandingOnShip))]
+        [HarmonyPatch(typeof(Character), nameof(Character.GetStandingOnShip)), HarmonyPrefix]
         private static bool Prefix(Character __instance, ref Ship __result)
         {
-            if (!__instance.IsOnGround())
-            {
-                __result = null;
-                return false;
-            }
-
-            if (__instance.m_lastGroundBody)
-            {
-                var cachedData = __instance.GetCachedShipData();
-
-                if (cachedData.IsCached)
-                {
-                    __result = cachedData.LastShip;
-                    return false;
-                }
-
-                cachedData.LastShip = __instance.m_lastGroundBody.GetComponent<Ship>();
-                cachedData.IsCached = true;
-
-                __result = cachedData.LastShip;
-                return false;
-            }
-
-            __result = null;
-            return false;
+	        if (!__instance.IsOnGround())
+	        {
+		        __result = null;
+		        return false;
+	        } 
+	       
+	        if (__instance.m_lastGroundBody)
+	        {
+		        LastShipByCharacter.TryGetValue(__instance, out var ship);
+		        __result = ship;
+		        return false;
+	        }
+	        
+	        __result = null;
+	        return false;
         }
 
+        [HarmonyPatch(typeof(Character), nameof(Character.UpdateGroundContact)), HarmonyPostfix]
+        private static void UpdateGroundContact_Postfix(Character __instance, float dt)
+        {
+	        if (__instance.m_lastGroundBody != null)
+	        {
+		        LastShipByCharacter[__instance] = __instance.m_lastGroundBody.GetComponent<Ship>();
+	        }
+	        else
+	        {
+		        LastShipByCharacter[__instance] = null;
+	        }
+        }
+
+        #if DEBUG
         [HarmonyPatch(typeof(Character), nameof(Character.FixedUpdate))]
         private static bool Prefix(Character __instance)
         {
-            var data = __instance.GetCachedShipData();
-            data.IsCached = false;
-            
             if (__instance.m_nview.IsValid())
             {
+	            Profiler.BeginSample("All");
+
 	            float fixedDeltaTime = Time.fixedDeltaTime;
+	            Profiler.BeginSample("layer");
 	            __instance.UpdateLayer();
+	            Profiler.EndSample();
 	            Profiler.BeginSample("cont effect");
 	            __instance.UpdateContinousEffects();
 	            Profiler.EndSample();
@@ -77,10 +82,14 @@ namespace ValheimPerformanceOptimizations.Patches
 	            __instance.UpdateGroundTilt(fixedDeltaTime);
 	            __instance.SetVisible(__instance.m_nview.HasOwner());
 	            Profiler.EndSample();
+	            Profiler.BeginSample("look trans");
 	            __instance.UpdateLookTransition(fixedDeltaTime);
+	            Profiler.EndSample();
 	            if (__instance.m_nview.IsOwner())
 	            {
+		            Profiler.BeginSample("ground contact");
 		            __instance.UpdateGroundContact(fixedDeltaTime);
+		            Profiler.EndSample();
 		            Profiler.BeginSample("noiz");
 		            __instance.UpdateNoise(fixedDeltaTime);
 		            Profiler.EndSample();
@@ -95,25 +104,17 @@ namespace ValheimPerformanceOptimizations.Patches
 		            Profiler.BeginSample("smoke");
 		            __instance.UpdateSmoke(fixedDeltaTime);
 		            Profiler.EndSample();
+		            Profiler.BeginSample("rest");
 		            __instance.UnderWorldCheck(fixedDeltaTime);
 		            __instance.SyncVelocity();
 		            __instance.CheckDeath();
+		            Profiler.EndSample();
 	            }
+	            Profiler.EndSample();
             }
 
-            return true;
+            return false;
         }
-
-        private class CachedShipData
-        {
-            public CachedShipData()
-            {
-                LastShip = null;
-                IsCached = false;
-            }
-
-            public Ship LastShip { get; set; }
-            public bool IsCached { get; set; }
-        }
+        #endif
     }
 }
