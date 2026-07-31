5dc2fd39b4e9778157ae179ae40e99b4499eeaf4
ontrigger <firewarpcd@gmail.com>
2026-07-31 19:33:10 +0300

    add new particle optimization prefab patch, temp remove others

--- ValheimPerformanceOptimizations/Patches/PrefabOptimization/PrefabPatches.cs
+++ ValheimPerformanceOptimizations/Patches/PrefabOptimization/PrefabPatches.cs
@@ -6,197 +6,240 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using VPO = ValheimPerformanceOptimizations.ValheimPerformanceOptimizations;
 
-namespace ValheimPerformanceOptimizations.Patches
+namespace ValheimPerformanceOptimizations.Patches;
+
+[HarmonyPatch]
+public static class PrefabPatches
 {
-	//[HarmonyPatch]
-	public static class PrefabPatches
+	private const float LeafParticleCullHeight = 0.79f;
+
+	private static readonly List<string> PrefabsWithLeafParticles = new()
 	{
-		private const float LeafParticleCullHeight = 0.79f;
+		//"Beech1", "Birch1_aut", "Birch2_aut", "Oak1",
+	};
 
-		private static readonly List<string> PrefabsWithLeafParticles = new()
+	private static readonly List<string> MaterialsWithDisabledInstancing = new List<string>
+	{
+		"Grill_mat", "meadbase", "poison", "poisonres_potion",
+		"stamina_potion", "portal_small",
+	}.Select(material => material + " (Instance)").ToList();
+
+	private static readonly List<string> PrefabsWithDisabledInstancing = new()
+	{
+		"piece_cookingstation", "MeadBaseFrostResist", "MeadBaseHealthMedium", "MeadBaseHealthMinor",
+		"MeadBasePoisonResist", "MeadBaseStaminaMedium", "MeadBaseStaminaMinor", "MeadBaseTasty",
+		"MeadPoisonResist", "MeadStaminaMedium", "MeadStaminaMinor", "portal_wood",
+	};
+
+	private static readonly List<string> PrefabsWithWastedMaterials = new()
+	{
+		/*"OLD_wood_roof_icorner", "wood_roof_icorner_45", "wood_roof_ocorner", "wood_roof_icorner",
+		"OLD_wood_roof_ocorner", "wood_roof_45", "wood_roof_top_45", "wood_roof_ocorner_45",
+		"wood_roof", "wood_roof_top", "OLD_wood_roof",*/
+	};
+
+	private static bool _isPatched;
+
+	/// <summary>
+	/// Some trees have leaf particles attached to them.
+	/// These particles get rendered at insane distances
+	/// where they have a screen size of a pixel at 1080p, while also not being instanced.
+	/// This patch inserts a new lod that will cull particles at a reasonable screen height.
+	/// It also makes the particles instanced, but it doesn't seem to instance more than 5
+	/// quads at a time for some reason
+	/// </summary>
+	private static void PatchPrefabWithLeaves(GameObject prefab)
+	{
+		var leafTransform = Utils.FindChild(prefab.transform, "leaf_particles");
+
+		var ps = leafTransform.GetComponent<ParticleSystem>();
+		var psRenderer = ps.GetComponent<ParticleSystemRenderer>();
+		psRenderer.renderMode = ParticleSystemRenderMode.Mesh;
+		psRenderer.enableGPUInstancing = true;
+
+		var lodGroup = prefab.GetComponent<LODGroup>();
+		if (!lodGroup)
 		{
-			"Beech1", "Birch1_aut", "Birch2_aut", "Oak1",
-		};
-
-		private static readonly List<string> MaterialsWithDisabledInstancing = new List<string>
-		{
-			"Grill_mat", "meadbase", "poison", "poisonres_potion",
-			"stamina_potion", "portal_small",
-		}.Select(material => material + " (Instance)").ToList();
-
-		private static readonly List<string> PrefabsWithDisabledInstancing = new()
-		{
-			"piece_cookingstation", "MeadBaseFrostResist", "MeadBaseHealthMedium", "MeadBaseHealthMinor",
-			"MeadBasePoisonResist", "MeadBaseStaminaMedium", "MeadBaseStaminaMinor", "MeadBaseTasty",
-			"MeadPoisonResist", "MeadStaminaMedium", "MeadStaminaMinor", "portal_wood",
-		};
-
-		private static readonly List<string> PrefabsWithWastedMaterials = new()
-		{
-			"OLD_wood_roof_icorner", "wood_roof_icorner_45", "wood_roof_ocorner", "wood_roof_icorner",
-			"OLD_wood_roof_ocorner", "wood_roof_45", "wood_roof_top_45", "wood_roof_ocorner_45",
-			"wood_roof", "wood_roof_top", "OLD_wood_roof",
-		};
-
-		private static bool _isPatched;
-
-		/// <summary>
-		/// Some trees have leaf particles attached to them.
-		/// These particles get rendered at insane distances
-		/// where they have a screen size of a pixel at 1080p, while also not being instanced.
-		/// This patch inserts a new lod that will cull particles at a reasonable screen height.
-		/// It also makes the particles instanced, but it doesn't seem to instance more than 5
-		/// quads at a time for some reason
-		/// </summary>
-		private static void PatchPrefabWithLeaves(GameObject prefab)
-		{
-			var leafTransform = Utils.FindChild(prefab.transform, "leaf_particles");
-
-			var ps = leafTransform.GetComponent<ParticleSystem>();
-			var psRenderer = ps.GetComponent<ParticleSystemRenderer>();
-			psRenderer.renderMode = ParticleSystemRenderMode.Mesh;
-			psRenderer.enableGPUInstancing = true;
-
-			var lodGroup = prefab.GetComponent<LODGroup>();
-			if (!lodGroup)
-			{
-				return;
-			}
-
-			LOD[] lods = lodGroup.GetLODs();
-			var lod0 = lods[0];
-
-			var prevHeight = lod0.screenRelativeTransitionHeight;
-			lod0.screenRelativeTransitionHeight = LeafParticleCullHeight;
-
-			Renderer[] noParticleRenderers = lod0.renderers
-				.Where(r => r.name != "leaf_particles")
-				.ToArray();
-
-			var newLods = new LOD[lods.Length + 1];
-
-			newLods[0] = lod0;
-			newLods[1] = new LOD(prevHeight, noParticleRenderers);
-			for (var index = 1; index < lods.Length; index++)
-			{
-				newLods[index + 1] = lods[index];
-			}
-
-			lodGroup.SetLODs(newLods);
+			return;
 		}
 
-		private static void PatchPrefabWithUninstancedMaterials(GameObject prefab)
+		LOD[] lods = lodGroup.GetLODs();
+		var lod0 = lods[0];
+
+		var prevHeight = lod0.screenRelativeTransitionHeight;
+		lod0.screenRelativeTransitionHeight = LeafParticleCullHeight;
+
+		Renderer[] noParticleRenderers = lod0.renderers
+			.Where(r => r.name != "leaf_particles")
+			.ToArray();
+
+		var newLods = new LOD[lods.Length + 1];
+
+		newLods[0] = lod0;
+		newLods[1] = new LOD(prevHeight, noParticleRenderers);
+		for (var index = 1; index < lods.Length; index++)
 		{
-			if (prefab == null) { return; }
-			foreach (var renderer in prefab.GetComponentsInChildren<Renderer>(true))
+			newLods[index + 1] = lods[index];
+		}
+
+		lodGroup.SetLODs(newLods);
+	}
+
+	private static void PatchPrefabWithUninstancedMaterials(GameObject prefab)
+	{
+		if (prefab == null) { return; }
+		foreach (var renderer in prefab.GetComponentsInChildren<Renderer>(true))
+		{
+			Material[] materials = renderer.sharedMaterials;
+			foreach (var material in materials)
 			{
-				Material[] materials = renderer.sharedMaterials;
-				foreach (var material in materials)
+				if (material != null)
 				{
-					if (material != null)
-					{
-						material.enableInstancing = true;
-					}
-					/*if (!MaterialsWithDisabledInstancing.Contains(material.name))
-					{
-						continue;
-					}
-
-					if (material.enableInstancing)
-					{
-						VPO.Logger.LogInfo($"material {material.name} is already instanced {prefab.name}");
-					}*/
+					material.enableInstancing = true;
 				}
-			}
-		}
-
-		private static void PatchPrefabWithWastedMaterials(GameObject prefab)
-		{
-			PrefabMaterialCombiner.CombinePrefabMaterials(prefab);
-		}
-
-		private static void PatchSnowStormParticle()
-		{
-			var environment = GameObject.Find("_GameMain/_Environment");
-
-			var luxLitShader = Shader.Find("Lux Lit Particles/ Bumped");
-
-			var snowStormParticles = Utils.FindChild(environment.transform, "SnowStorm");
-			var snow = snowStormParticles.GetChild(1).gameObject;
-
-			var snowPs = snow.GetComponent<ParticleSystem>();
-			var snowPsMain = snowPs.main;
-			snowPsMain.maxParticles = 9000;
-
-			var minMaxCurve = snowPsMain.startSize;
-			minMaxCurve.mode = ParticleSystemCurveMode.TwoConstants;
-			minMaxCurve.constantMin = 0.03f;
-			minMaxCurve.constantMax = 0.07f;
-			snowPsMain.startSize = minMaxCurve;
-
-			var snowPsEmission = snowPs.emission;
-			var rateOverTime = snowPsEmission.rateOverTime;
-			rateOverTime.constant = 3000f;
-			snowPsEmission.rateOverTime = rateOverTime;
-
-			var snowPsShape = snowPs.shape;
-			snowPsShape.radiusThickness = 1f;
-
-			var snowPsRenderer = snow.GetComponent<ParticleSystemRenderer>();
-			var snowClusterMaterial = snowPsRenderer.material;
-			snowClusterMaterial.mainTexture = null;
-			snowClusterMaterial.DisableKeyword("GEOM_TYPE_FROND");
-		}
-
-		private static int PatchPrefabs(
-			this IReadOnlyDictionary<int, GameObject> allPrefabs, IEnumerable<string> prefabNames,
-			Action<GameObject> patcher)
-		{
-			var totalPatched = 0;
-			foreach (var prefabName in prefabNames)
-			{
-				if (allPrefabs.TryGetValue(prefabName.GetStableHashCode(), out var prefab))
+				/*if (!MaterialsWithDisabledInstancing.Contains(material.name))
 				{
-					patcher(prefab);
-					totalPatched += 1;
+					continue;
 				}
-			}
 
-			return totalPatched;
+				if (material.enableInstancing)
+				{
+					VPO.Logger.LogInfo($"material {material.name} is already instanced {prefab.name}");
+				}*/
+			}
 		}
+	}
 
-		[HarmonyPatch(typeof(ZNetScene), nameof(ZNetScene.Awake))]
-		private static void Postfix(ZNetScene __instance, Dictionary<int, GameObject> ___m_namedPrefabs)
+	private static void PatchPrefabWithWastedMaterials(GameObject prefab)
+	{
+		PrefabMaterialCombiner.CombinePrefabMaterials(prefab);
+	}
+
+	private static int PatchLocalParticleSystemCulling(GameObject prefab)
+	{
+		var patched = 0;
+		foreach (var particleSystem in prefab.GetComponentsInChildren<ParticleSystem>(true))
 		{
-			if (_isPatched)
-			{
-				return;
-			}
+			var main = particleSystem.main;
 
-			Dictionary<int, GameObject> namedPrefabs = __instance.m_namedPrefabs;
 
-			var patched = 0;
-			patched += namedPrefabs.PatchPrefabs(PrefabsWithLeafParticles, PatchPrefabWithLeaves);
-			//patched += namedPrefabs.PatchPrefabs(PrefabsWithDisabledInstancing, PatchPrefabWithUninstancedMaterials);
-			foreach (var prefab in namedPrefabs.Values)
+			var cullingMode = particleSystem.gameObject.name switch
 			{
-				PatchPrefabWithUninstancedMaterials(prefab);
-				patched += 1;
-			}
-			if (SystemInfo.graphicsDeviceType != GraphicsDeviceType.Null)
-			{
-				var now = DateTime.Now;
-				patched += namedPrefabs.PatchPrefabs(PrefabsWithWastedMaterials, PatchPrefabWithWastedMaterials);
-				ValheimPerformanceOptimizations.Logger.LogInfo("Combined prefab mats in "
-					+ (DateTime.Now - now).TotalMilliseconds + " ms");
-			}
+				"flare" => ParticleSystemCullingMode.Pause,
+				"flames" => ParticleSystemCullingMode.PauseAndCatchup,
+				"flames (1)" => ParticleSystemCullingMode.PauseAndCatchup,
+				"embers" => ParticleSystemCullingMode.PauseAndCatchup,
+				"low_flames" => ParticleSystemCullingMode.PauseAndCatchup,
+				"sparcs (1)" => ParticleSystemCullingMode.PauseAndCatchup,
+				"smoke (1)" => ParticleSystemCullingMode.PauseAndCatchup,
+				_ => main.cullingMode,
+			};
 
-			PatchSnowStormParticle();
+			main.cullingMode = cullingMode;
+			// necessary to make the reflection renderer not re-simulate particles
+			particleSystem.gameObject.layer = LayerMask.NameToLayer("TransparentFX");
 			patched += 1;
-
-			ValheimPerformanceOptimizations.Logger.LogInfo($"Patched {patched} prefabs");
-
-			_isPatched = true;
 		}
+
+		return patched;
+	}
+
+	private static string GetPrefabParticlePath(Transform particleTransform)
+	{
+		var path = particleTransform.name;
+		var currentTransform = particleTransform;
+		while (currentTransform.parent != null)
+		{
+			currentTransform = currentTransform.parent;
+			path = currentTransform.name + "/" + path;
+		}
+
+		return path;
+	}
+
+	private static void PatchSnowStormParticle()
+	{
+		var environment = GameObject.Find("_GameMain/_Environment");
+
+		var luxLitShader = Shader.Find("Lux Lit Particles/ Bumped");
+
+		var snowStormParticles = Utils.FindChild(environment.transform, "SnowStorm");
+		var snow = snowStormParticles.GetChild(1).gameObject;
+
+		var snowPs = snow.GetComponent<ParticleSystem>();
+		var snowPsMain = snowPs.main;
+		snowPsMain.maxParticles = 9000;
+
+		var minMaxCurve = snowPsMain.startSize;
+		minMaxCurve.mode = ParticleSystemCurveMode.TwoConstants;
+		minMaxCurve.constantMin = 0.03f;
+		minMaxCurve.constantMax = 0.07f;
+		snowPsMain.startSize = minMaxCurve;
+
+		var snowPsEmission = snowPs.emission;
+		var rateOverTime = snowPsEmission.rateOverTime;
+		rateOverTime.constant = 3000f;
+		snowPsEmission.rateOverTime = rateOverTime;
+
+		var snowPsShape = snowPs.shape;
+		snowPsShape.radiusThickness = 1f;
+
+		var snowPsRenderer = snow.GetComponent<ParticleSystemRenderer>();
+		var snowClusterMaterial = snowPsRenderer.material;
+		snowClusterMaterial.mainTexture = null;
+		snowClusterMaterial.DisableKeyword("GEOM_TYPE_FROND");
+	}
+
+	private static int PatchPrefabs(
+		this IReadOnlyDictionary<int, GameObject> allPrefabs, IEnumerable<string> prefabNames,
+		Action<GameObject> patcher)
+	{
+		var totalPatched = 0;
+		foreach (var prefabName in prefabNames)
+		{
+			if (allPrefabs.TryGetValue(prefabName.GetStableHashCode(), out var prefab))
+			{
+				patcher(prefab);
+				totalPatched += 1;
+			}
+		}
+
+		return totalPatched;
+	}
+
+	[HarmonyPatch(typeof(ZNetScene), nameof(ZNetScene.Awake))]
+	private static void Postfix(ZNetScene __instance, Dictionary<int, GameObject> ___m_namedPrefabs)
+	{
+		if (_isPatched) { return; }
+
+		Dictionary<int, GameObject> namedPrefabs = __instance.m_namedPrefabs;
+
+		var patched = 0;
+		var particleCullingPatched = 0;
+		patched += namedPrefabs.PatchPrefabs(PrefabsWithLeafParticles, PatchPrefabWithLeaves);
+		//patched += namedPrefabs.PatchPrefabs(PrefabsWithDisabledInstancing, PatchPrefabWithUninstancedMaterials);
+		foreach (var prefab in namedPrefabs.Values)
+		{
+			PatchPrefabWithUninstancedMaterials(prefab);
+			particleCullingPatched += PatchLocalParticleSystemCulling(prefab);
+			patched += 1;
+		}
+		if (SystemInfo.graphicsDeviceType != GraphicsDeviceType.Null)
+		{
+			var now = DateTime.Now;
+			patched += namedPrefabs.PatchPrefabs(PrefabsWithWastedMaterials, PatchPrefabWithWastedMaterials);
+			VPO.Logger.LogInfo("Combined prefab mats in "
+				+ (DateTime.Now - now).TotalMilliseconds + " ms");
+		}
+
+		PatchSnowStormParticle();
+		patched += 1;
+
+		VPO.Logger.LogInfo(
+			$"Updated culling for {particleCullingPatched} local-space flare or flame particle systems"
+		);
+		VPO.Logger.LogInfo($"Patched {patched} prefabs");
+
+		_isPatched = true;
 	}
 }
